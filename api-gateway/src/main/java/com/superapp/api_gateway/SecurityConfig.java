83570f401a0ff30a1c87473e85aa16c3839f7081
kipfuho <ctcakip@gmail.com>
2025-08-30 23:55:11 +0700

    fix: gateway

--- api-gateway/src/main/java/com/superapp/api_gateway/SecurityConfig.java
+++ api-gateway/src/main/java/com/superapp/api_gateway/SecurityConfig.java
@@ -2,20 +2,20 @@ package com.superapp.api_gateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-class SecurityConfig {
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-        // .authorizeHttpRequests(auth -> auth
-        // .requestMatchers("/actuator/health", "/actuator/info").permitAll()
-        // .anyRequest().authenticated())
-        // .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
-        return http.build();
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(ex -> ex
+                        .anyExchange().permitAll())
+                .build();
     }
 }
