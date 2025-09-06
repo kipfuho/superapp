19e292cb1803509f4b7ef38f20ed0e9be5474dbc
kipfuho <ctcakip@gmail.com>
2025-09-06 11:59:57 +0700

    feat: pos-order-system services

---
+++ pos-order-system/cdp-service/src/main/java/com/superapp/cdp_service/CdpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.superapp.cdp_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdpServiceApplication.class, args);
+	}
+
+}
---
+++ pos-order-system/cdp-service/src/test/java/com/superapp/cdp_service/CdpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superapp.cdp_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
